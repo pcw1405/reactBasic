e536b69a11ac680cd50ddba6de3cd87962c00085
chungwon <pcw14057@gmail.com>
2024-02-24 17:32:30 +0900

    modal

--- src/App.js
+++ src/App.js
@@ -40,10 +40,26 @@ function App() {
         <p>2월 17일 발행 </p>
         <hr/>
       </div>
+      
+      <Modal />
 
+    
     </div>
     
   );
+
+}
+function Modal(){
+  return (
+    <>
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+    <div></div>
+    </>
+  )
 }
 
 export default App;
